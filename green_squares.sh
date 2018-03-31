ab4d9e3c6dacc1c11c3d4578314faefe372b418d
vipyne <vipyne@gmail.com>
2018-03-31 21:10:43 +0900

    put grafitti design string transposition inside bash script

--- green_squares.sh
+++ green_squares.sh
@@ -4,6 +4,29 @@
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
+GRAFITTI_DESIGN=$(ruby -e " string_w_newlines = '
+1111191119111118111181888881888881811111111181111111
+1111999199911118111181118111118111811111111888111111
+1111999999911118111181118111118111811118888888888811
+1111999999911118111181118111118111811111188888881111
+1111199999111118111181118111118111811111118818811111
+1111119991111118111181118111118111811111188111881111
+1111111911111118888181118111118111888811811111118111
+'
+height = 7 # days
+width = 52 # weeks
+in_string = string_w_newlines.gsub(/\n/, '')
+out_string = in_string.clone
+width.times do |ww|
+  height.times do |hh|
+    out_string[height * ww + hh] = in_string[width * hh + ww]
+  end
+end
+puts out_string += '1111111' # add a couple extra days for in progress week
+")
+
+echo $GRAFITTI_DESIGN
+
 if [[ "${#@}" -lt 2 ]]; then
   echo " ##"
   echo " #  Please add at least 2 args"
@@ -90,12 +113,10 @@ git init
 git add .
 git commit -m "initial commit of the script responsible for drawing pictures with green squares"
 
-STRING="1111111111111111111111111111199911199999111999991119999919999919999911199911111111111111111111111111111119999999111111911111191111119111111199999991111111911111191111119999999911111191111111111111911111191111119999999911111191111111111111999999911111191111119111111911111111191111119111911991911199991199991199991111999911119999111991911191119119111111111111111111"
-
 rewriteHistory() {
   WORKING=true
-  for i in $(seq 1 ${#STRING}); do
-    INNERLOOP="${STRING:i-1:1}"
+  for i in $(seq 1 ${#GRAFITTI_DESIGN}); do
+    INNERLOOP="${GRAFITTI_DESIGN:i-1:1}"
     for j in $( seq 1 $(($INNERLOOP * 4)) ); do
       # # github is *not* liking how many files this produces:
       touch "$i-$j-unixtime" # sorry github
